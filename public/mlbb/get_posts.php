5454a1216a4c71a9f957f806b60c04b575000ae8
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-01-29 22:24:19 +0200

    fixed all get_posts.php

--- public/mlbb/get_posts.php
+++ public/mlbb/get_posts.php
@@ -4,16 +4,5 @@ require "db.php";
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:5173");
 
-$table = $_GET['table'] ?? 'posts'; // по умолчанию posts
-
-// белый список, чтобы не дать выполнить любой SQL
-$allowed = ['chat_members', 'posts', 'news'];
-
-if (!in_array($table, $allowed)) {
-  http_response_code(400);
-  echo json_encode(["error" => "Invalid table"]);
-  exit;
-}
-
-$stmt = $pdo->query("SELECT * FROM $table ORDER BY id DESC");
+$stmt = $pdo->query("SELECT * FROM chat_members ORDER BY id DESC");
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
