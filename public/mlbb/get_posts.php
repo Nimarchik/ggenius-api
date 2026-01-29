28b3605a9796cc5b53285e1651f32a732c8e8480
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-01-29 22:55:55 +0200

    add posts/table

--- public/mlbb/get_posts.php
+++ public/mlbb/get_posts.php
@@ -7,7 +7,29 @@ header("Access-Control-Allow-Origin: http://localhost:5173");
 $table = $_GET['table'] ?? 'posts'; // по умолчанию posts
 
 // белый список, чтобы не дать выполнить любой SQL
-$allowed = ['chat_members', 'posts', 'news'];
+$allowed = [
+  'chat_members',
+  'posts',
+  'news',
+  'arena_stats',
+  'arena_defense',
+  'arena_battle_log',
+  'tournaments',
+  'tournament_teams',
+  'tournament_team_members',
+  'tournament_matches',
+  'tournament_registrations',
+  'tournament_solo_registrations',
+  'tournament_team_join_requests',
+  'stream_requests',
+  'squads',
+  'squad_members',
+  'squad_applications',
+  'squad_invitations',
+  'party_lobbies',
+  'party_lobby_members',
+  'duel_stats'
+];
 
 if (!in_array($table, $allowed)) {
   http_response_code(400);
