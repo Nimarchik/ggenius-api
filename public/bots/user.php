914b4f5e5d0d74edea5c0532b3f3c39e56b7f9e6
nimarchik <lexaxiip@gmail.com>
2026-02-03 01:20:57 +0200

    fix cors

--- public/bots/user.php
+++ public/bots/user.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
 
 // Подключение к базе
 $dbUrl = getenv('DATABASE_URL');
