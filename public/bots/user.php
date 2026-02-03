b884cb17c64aa55f7c5305e02c9b12b4672bbb13
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-02-03 09:21:16 +0200

    add CORS to user.php

--- public/bots/user.php
+++ public/bots/user.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+
 // Подключение к базе
 $dbUrl = getenv('DATABASE_URL');
 $dbopts = parse_url($dbUrl);
