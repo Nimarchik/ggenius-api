e4f0ada4fb8e0fb698513eb0290cc1f65d156c23
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-02-05 11:48:49 +0200

    fix redis

--- public/redis/index.php
+++ public/redis/index.php
@@ -1,13 +1,13 @@
 <?php
 $redis = new Redis();
 
-$redis_url = getenv('REDIS_URL');
+$redis_url = getenv('REDIS_URL'); // например, REDIS_URL=redis://default:password@host:port
 $parts = parse_url($redis_url);
 
 $host = $parts['host'];
 $port = $parts['port'];
 $password = $parts['pass'] ?? null;
-$db = $parts['path'] ? ltrim($parts['path'], '/') : 0;
+$db = isset($parts['path']) ? ltrim($parts['path'], '/') : 0;
 
 if (!$redis->connect($host, $port)) {
   die("Не удалось подключиться к Redis");
