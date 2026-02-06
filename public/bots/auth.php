b2c622ae4fdbed8a352ace56482e6817d8697e30
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-02-06 16:44:53 +0200

    fix url auth

--- public/bots/auth.php
+++ public/bots/auth.php
@@ -105,6 +105,6 @@ pg_query_params(
 );
 
 // Редирект на фронт
-$frontend = getenv('FRONTEND_URL') ?: 'https://0416-109-229-4-233.ngrok-free.app';
+$frontend = getenv('FRONTEND_URL') ?: 'http://localhost:5173';
 header("Location: {$frontend}?access={$accessToken}&refresh={$refreshToken}");
 exit;
